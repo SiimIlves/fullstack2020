a943f8c6a0d3ba70c4b4a2cf9792f12346da4203
SiimIlves <65825538+SiimIlves@users.noreply.github.com>
2021-01-12 14:01:08 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Part = (props) => {
+  return (
+  <p>{props.part} {props.exercises}</p>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+      <Part part1={props.part1} exercises1={props.exercises1} />
+      <Part part2={props.part2} exercises2={props.exercises2} />
+      <Part part3={props.part3} exercises3={props.exercises3} />
+      </p>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <p> Number of exercises {props.part1.exercises1 + props.part2.exercises2 + props.part3.exercises3} </p>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total Number of exercises {...exercises1 + exercises2 + exercises3} />
+      </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
